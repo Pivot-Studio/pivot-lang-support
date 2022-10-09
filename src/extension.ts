873be5fd5ee8c3f8c2ace17a0312f383f349ba27
1360151219 <1360151219@qq.com>
2022-10-09 12:01:05 +0800

    feat: add keyword scope

--- src/extension.ts
+++ src/extension.ts
@@ -42,7 +42,7 @@ export function activate(context: ExtensionContext) {
 		},
 		outputChannelName: "pivot-lang language server",
 	};
-
+	
 	// Create the language client and start the client.
 	client = new LanguageClient(
 		'pivot-langlanguageServer',
